938fac9d9ccca69284531d8c3bbb7edd4d4369cd
Jeremy Fabre <jeremy.fabre@hotmail.com>
2011-03-06 20:09:52 -0500

    changed all erbs to haml

--- lib/MaximumPointsValidator.rb
+++ lib/MaximumPointsValidator.rb
@@ -1,14 +1,11 @@
 require 'SprintProgressValidator'
 class MaximumPointsValidator < SprintProgressValidator
   def validate_progresses(sprint, progresses)
-    Rails.logger.info " --- > TRIED TO VALIDATE"
     if progresses.nil? || !progresses.respond_to?('to_a') || !sprint.respond_to?('total_points')
-      Rails.logger.info " --- > SOMETHING WAS WRONG"
       return
     end 
     progresses_points = progresses.sum{|x| x.points }
     
-    Rails.logger.info " progresses_points > (sprint.total_points || 0) ? #{progresses_points > (sprint.total_points || 0)}"
     if progresses_points > (sprint.total_points || 0)
       yield
     end
